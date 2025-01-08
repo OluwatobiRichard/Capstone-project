a8200f4d72683f91df1434447a80c4ddc23f8ef2
OluwatobiRichard <oluwatobirichard001@gmail.com>
2025-01-08 19:18:49 +0100

    database

--- Movie-database/src/components/Hero/Hero.jsx
+++ Movie-database/src/components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import  MySvg from '../../assets/background.svg'
 
 const hero = () => {
   return (
-    <div>
-      
+    <div className="bg-center h-screen"
+          style={{
+            backgroundImage: `url(${MySvg})`,
+            backgroundSize: '150%', // Adjust this value to increase the size
+            backgroundRepeat: 'no-repeat'
+          }}
+    >
+      <div className='flex items-center justify-center h-full'>
+        <h1 className='text-white'>Explore a world of films with our extensive collection.</h1>
+      </div>
     </div>
   )
 }
